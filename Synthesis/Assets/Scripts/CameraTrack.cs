09f1eb5e82e2a205dc3f47d69c8ec6d82b305a03
Ryan Fieldhouse <rcfieldhouse@yahoo.com>
2022-06-12 18:01:41 -0400

    camera tracks player

---
+++ Synthesis/Assets/Scripts/CameraTrack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    
+    private Rigidbody2D m_Rigidbody2D;
+    private Vector3 difference = new Vector3(0.0f, 0.0f, 10.0f);
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = target.position - difference;
+    }
+}
